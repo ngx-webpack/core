b1c805f14b7ebbe74d9933300caa50dd907861e6
gsoos <soos.gabor86@gmail.com>
2017-05-16 20:52:41 +0200

    remove dependency on ngx-karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,63 @@
 'use strict';
 
-let karmaGenerator = require('ngx-karma').karma;
+let path = require('path');
 let webpackConfig = require('./webpack.config');
-let Config = require('./lib/config');
-let config = new Config();
+let polyfills = [
+  'ts-helpers',
+  'core-js/client/shim',
+  'zone.js/dist/zone',
+  'zone.js/dist/long-stack-trace-zone',
+  'zone.js/dist/async-test',
+  'zone.js/dist/fake-async-test',
+  'zone.js/dist/sync-test',
+  'zone.js/dist/proxy',
+  'zone.js/dist/jasmine-patch'
+];
 
-let { singleRun, useDlls } = config.calculate();
-let karmaConfig = karmaGenerator(webpackConfig, { singleRun, useDlls });
+let karmaGenerator = function(webpackConfig, { useDlls }) {
+  let testFile = { pattern: 'src/app/test.ts' };
+  let dllFile = { pattern: 'src/dll/angular.dll.js' };
+
+  let polyfillFiles = polyfills
+    .map(file => {
+      return { pattern: require.resolve(file) };
+    });
+
+  let files = useDlls
+    ? polyfillFiles.concat([dllFile, testFile])
+    : polyfillFiles.concat([testFile]);
+
+  return {
+    basePath: process.cwd(),
+
+    frameworks: ['jasmine'],
+
+    files,
+
+    preprocessors: {
+      'src/app/test.ts': ['webpack', 'sourcemap']
+    },
+
+    reporters: ['dots'],
+
+    webpack: webpackConfig,
+    webpackServer: { noInfo: true },
+
+    port: 9876,
+
+    colors: true,
+
+    logLevel: 'INFO',
+
+    browsers: ['PhantomJS'],
+
+    autoWatch: true,
+    singleRun: false
+  };
+};
 
 module.exports = (config) => {
-  config.set(karmaConfig);
+  config.set(
+    karmaGenerator(webpackConfig, { useDlls: true })
+  );
 };
